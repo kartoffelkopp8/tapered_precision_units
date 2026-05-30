f841aa42df0451d3327683890d7243ecd5a32824
felix <felix.blenk@fau.de>
2026-05-30 20:08:37 +0200

    only 8 errors remaining

--- mul/takum_logarithmic_encoder.vhd
+++ mul/takum_logarithmic_encoder.vhd
@@ -20,23 +20,23 @@ entity takum_logarithmic_encoder is
 end entity takum_logarithmic_encoder;
 
 architecture RTL of takum_logarithmic_encoder is
-    signal s_c_raw    : std_logic_vector(8 downto 0); 
+    signal s_c_raw    : std_logic_vector(8 downto 0); -- c with sign 
     signal s_c_wo_d   : std_logic_vector(7 downto 0);
-    signal s_mant_raw : std_logic_vector(G_N + 3 - 9 downto 0); 
+    signal s_mant_raw : std_logic_vector(G_N + 3 - 9 downto 0); -- mantissa cutut from input
     signal s_dir      : std_logic;
 
     signal s_cond_inv_c : std_logic_vector(7 downto 0);
-    signal s_precursor  : std_logic_vector(7 downto 0); 
+    signal s_precursor  : std_logic_vector(7 downto 0); -- precursor has  first 1 at value 2^r
 
-    signal s_regime_tmp      : std_logic_vector(2 downto 0);
-    signal s_regime          : std_logic_vector(2 downto 0);
-    signal s_regime_cond_inv : std_logic_vector(2 downto 0);
-
+    signal s_regime_tmp         : std_logic_vector(2 downto 0);
+    signal s_regime             : std_logic_vector(2 downto 0);
+    signal s_regime_cond_inv    : std_logic_vector(2 downto 0);
     signal s_precursor_cond_inv : std_logic_vector(6 downto 0);
 
     signal s_vld : std_logic;
 
-    signal s_c_mant_pre_shift : std_logic_vector(G_N + 9 - 1 downto 0); 
+    -- signal s_c_mant_pre_shift : std_logic_vector(s_mant_raw'length + 8-1 downto 0); -- append characteristics to mantissa for shift
+    signal s_c_mant_pre_shift : std_logic_vector(G_N + 9 - 1 downto 0); -- append characteristics to mantissa for shift
     signal s_c_mant_shftd     : std_logic_vector(G_N + 9 - 1 downto 0);
     signal s_c_m              : std_logic_vector(G_N + 2 - 1 downto 0);
 
@@ -47,18 +47,23 @@ architecture RTL of takum_logarithmic_encoder is
     signal s_guard_bit  : std_logic;
     signal s_sticky_bit : std_logic;
 
-    signal s_round_up : std_logic;
-    
-    signal s_takum_rounded : std_logic_vector(G_N - 1 downto 0);
+    signal s_round_up_overflow    : std_logic; --marks if round up would overflow the takum
+    signal s_round_down_underflow : std_logic; --marks if round down would underflow the takum
+    signal s_round_up             : std_logic;
+
+    signal s_takum_rounded : std_logic_vector(G_N-1 downto 0);
 begin
 
     s_c_raw    <= i_l(G_N + 3 downto G_N - 5);
     s_mant_raw <= i_l(G_N - 5 - 1 downto 0);
-    s_dir      <= not(s_c_raw(8));
+    s_dir      <= not (s_c_raw(8));
 
-    s_c_wo_d     <= s_c_raw(7 downto 0);
+    -- throw away sign, only used for d calculation
+    s_c_wo_d <= s_c_raw(7 downto 0);
+
     s_cond_inv_c <= cond_invert(s_c_wo_d, not (s_dir));
-    s_precursor  <= std_logic_vector(unsigned(s_cond_inv_c) + 1); 
+
+    s_precursor <= std_logic_vector(unsigned(s_cond_inv_c) + 1); -- ountil here equal
 
     LOD : entity work.LOD
         generic map(
@@ -70,7 +75,8 @@ begin
             o_vld => s_vld
         );
 
-    s_regime          <= not(s_regime_tmp);
+    -- s_regime          <= s_regime_tmp when s_vld = '1' else "111"; -- not sure, sollt eghoist bits als 0 annehmen
+    s_regime          <= not (s_regime_tmp);
     s_regime_cond_inv <= cond_invert(s_regime, not(s_dir));
 
     s_precursor_cond_inv <= cond_invert(s_precursor(6 downto 0), not (s_dir));
@@ -87,22 +93,57 @@ begin
             i_shift_amount => s_regime,
             o_vec          => s_c_mant_shftd
         );
-    s_c_m <= s_c_mant_shftd(s_c_mant_shftd'high - 7 downto 0); 
+    s_c_m <= s_c_mant_shftd(s_c_mant_shftd'high - 7 downto 0); -- take lower bits
 
-    s_pre_round <= i_sign & s_dir & s_regime_cond_inv & s_c_m; 
+    s_pre_round <= i_sign & s_dir & s_regime_cond_inv & s_c_m; -- between E3 and E4 in paper
 
-    -- up to here: equal 
+    calculate_ov_un_flow : process(i_l) is
+        constant C_UNDERFLOW_THRESHOLD : characteristic := get_underflow_value(G_N);
+        constant C_OVERFLOW_THRESHOLD  : characteristic := get_overflow_value(G_N);
 
+        variable v_char_value : integer;
+    begin
+        v_char_value := to_integer(signed(i_l(G_N + 3 downto G_N - 5)));
+
+        if G_N <= 11 then
+            if (v_char_value <= C_UNDERFLOW_THRESHOLD) then
+                s_round_down_underflow <= '1';
+            else
+                s_round_down_underflow <= '0';
+            end if;
+
+            if (v_char_value >= C_OVERFLOW_THRESHOLD) then
+                s_round_up_overflow <= '1';
+            else
+                s_round_up_overflow <= '0';
+            end if;
+        else
+            if (i_l(G_N - 6 downto 0) = (i_l(G_N - 6 downto 0)'range => '0')) and (v_char_value = C_UNDERFLOW_THRESHOLD) then
+                s_round_down_underflow <= '1';
+            else
+                s_round_down_underflow <= '0';
+            end if;
+
+            if (i_l(G_N - 6 downto 0) = (i_l(G_N - 6 downto 0)'range => '1')) and (v_char_value = C_OVERFLOW_THRESHOLD) then
+                s_round_up_overflow <= '1';
+            else
+                s_round_up_overflow <= '0';
+            end if;
+        end if;
+    end process calculate_ov_un_flow;
+
+    -- rounding logic
     s_takum_nr   <= s_pre_round(G_N + 7 - 1 downto 7);
     s_lsb_bit    <= s_takum_nr(s_takum_nr'low);
     s_guard_bit  <= s_pre_round(6);
     s_sticky_bit <= or_reduce(s_pre_round(5 downto 0));
 
-    s_round_up <= (s_guard_bit and (s_lsb_bit or s_sticky_bit));
+    -- s_round_up <= (s_guard_bit and (s_lsb_bit or s_sticky_bit));
+    s_round_up <= (s_round_down_underflow) or (not(s_round_up_overflow) and s_guard_bit and (s_lsb_bit or s_sticky_bit));
+
 
     s_takum_rounded <= std_logic_vector(unsigned(s_takum_nr) + unsigned(std_logic_vector'(0 => s_round_up)));
-
-    saturation : process(s_takum_rounded, i_overflow, i_underflow, i_sign) is
+saturation : process(s_takum_rounded, i_overflow, i_underflow, i_sign) is
         variable v_max_takum : std_logic_vector(G_N-1 downto 0);
         variable v_min_takum : std_logic_vector(G_N-1 downto 0);
     begin
@@ -117,5 +158,5 @@ begin
             o_takum <= s_takum_rounded;
         end if;
     end process saturation;
-
+    -- o_takum <= s_takum_rounded;
 end architecture RTL;
